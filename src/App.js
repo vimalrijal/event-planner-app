97de02fda088dc485d433a1c23cc327f3add35bd
vimal rijal <106002509+vimalrijal@users.noreply.github.com>
2022-09-24 15:46:21 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
+import React,{useState} from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //components
 import {Create, Home, Event} from './components';
 
+export const Store = React.createContext();
+
 function App() {
+  const [data, setdata] = useState({
+    eventName: '',
+    hostName: '',
+    location:'',
+    startdate:'',
+    endDate:''
+  })
+
   return (
-    <>
+    <Store.Provider value={{data, setdata}}>
      <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
@@ -14,8 +25,9 @@ function App() {
         <Route path="/event" element={<Event/>}/>
       </Routes>
     </BrowserRouter>
-    </>
+    </Store.Provider>
   );
 }
 
 export default App;
+
