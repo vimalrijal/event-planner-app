4f0952d1e2c09faed1d6d39a631386a2f598de69
vimal rijal <106002509+vimalrijal@users.noreply.github.com>
2022-09-24 15:47:59 +1000

    Update Event.jsx

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
+import React,{useContext} from 'react'
 
 import cakeImg from '../assets/images/Birthday-cake.png'
 
 import { HiOutlineCalendar,HiLocationMarker,HiChevronRight } from "react-icons/hi";
+import { Store } from '../App';
+
+
 
 function Event() {
+    const datas = useContext(Store);
+    console.log('sddfsd', datas);
   return (
     <div className='w-full h-screen flex justify-between bg-purple-50'>
-
         <div className='flex flex-col ml-[240px]'>
 
             <h1 className='text-5xl mt-[184px] text-center text-violet-900'>Birthday Bash</h1>
-            <p className='mt-[5px] text-gray-500'>Hosted by <span className='font-bold'>Vimal Rijal</span></p>
+            <p className='mt-[5px] text-gray-500'>Hosted by <span className='font-bold'>{datas.data.hostName}</span></p>
 
             <div className='flex flex-col ml-[10px] mt-[25px]'>
                 <div className='flex justify-between'>
                 <HiOutlineCalendar/>
                 <div className='flex flex-col '>
-                    <h1 className='text-violet-900 font-bold'>18 august 6:00pm</h1>
-                    <h1>to 19 august 1:00pm</h1>
+                    <h1 className='text-violet-900 font-bold'>{datas.data.startDate}</h1>
+                    <h1>{datas.data.endDate}</h1>
                 </div>
                 <HiChevronRight/>
                 </div>
@@ -26,7 +30,7 @@ function Event() {
                 <HiLocationMarker/>
                 <div className='flex flex-col '>
                     <h1 className='text-violet-900 font-bold'>Street name</h1>
-                    <h1>Suburb, postcode, street</h1>
+                    <h1>{datas.data.location}</h1>
                 </div>
                 <HiChevronRight/>
                 </div>
